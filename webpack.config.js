b2f708fe8ac104b756714f8aa8899deea944ea83
audthenerd <aud.kow88@gmail.com>
2018-11-30 18:15:32 +0800

    added another version of webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
+const DIR = require("path").resolve(__dirname)
+const PROD = process.env.NODE_ENV === "production"
+
 module.exports = {
-    entry: './index.js',
-    output: {
-        filename: '[name].bundle.js',
-        path: __dirname,
-        publicPath: __dirname
-    }
-};
+  entry: {
+    application: `${DIR}/app/assets/javascripts/application.js`,
+  },
+  mode: PROD ? "production" : "development",
+  output: {
+    chunkFilename: "[name].js",
+    filename: "[name].js",
+    path: `${DIR}public/assets/`,
+  },
+  resolve: {
+    extensions: [".js", ".json", ".css"],
+    modules: ["node_modules", `${DIR}/app/assets`],
+  },
+}
